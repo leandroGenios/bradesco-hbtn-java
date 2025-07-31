41becb8a002c3bc8a7be47d103b2860edd5ce062
Leandro Moreira <leandro.genios@gmail.com>
2025-07-31 17:37:37 -0300

    ajuste

--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,5 +1,3 @@
-package com.example.demo;
-
 public class PersonagemGame {
 
 	public int saudeAtual;
--- Classes_Objetos/personagem/Program.java
+++ Classes_Objetos/personagem/Program.java
@@ -1,5 +1,3 @@
-package com.example.demo;
-
 public class Program {
 	public static void main(String[] args) {
         PersonagemGame personagem1 = new PersonagemGame();
