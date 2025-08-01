37efba03ebe276714edaa7de1b10a8630be66ec8
Leandro Moreira <leandro.genios@gmail.com>
2025-07-31 21:13:16 -0300

    status

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,44 @@
+public class PersonagemGame {
+
+	private int saudeAtual;
+	private String nome;
+	private String status;
+	
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+		if(this.saudeAtual > 0) {
+			this.status = "vivo";
+		}else {
+			this.status = "morto";
+		}
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void tomarDano (int quantidadeDeDano) {
+		setSaudeAtual(getSaudeAtual() - quantidadeDeDano);
+		
+		if(getSaudeAtual() < 0) {
+			setSaudeAtual(0);
+		}
+	}
+	
+	public void receberCura (int quantidadeDeCura) {
+		setSaudeAtual(getSaudeAtual() + quantidadeDeCura);
+		
+		if(getSaudeAtual() > 100) {
+			setSaudeAtual(100);
+		}
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+}
