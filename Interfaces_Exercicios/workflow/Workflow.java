60d2d7b7553136bf1b1684347238e2fac804fe51
Leandro Moreira <leandro.genios@gmail.com>
2025-08-17 21:25:46 -0300

    interface exercicios zero

---
+++ Interfaces_Exercicios/workflow/Workflow.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import atividades.Atividade;
+
+public class Workflow {
+    private List<Atividade> atividades = new ArrayList<>();
+
+    public void registrarAtividade(Atividade atividade) {
+        atividades.add(atividade);
+    }
+
+    public List<Atividade> getAtividades() {
+        return atividades;
+    }
+}
