b2f3851115e545b82969bfb02eaefcbdbf408afc
Leandro Moreira <leandro.genios@gmail.com>
2025-08-25 22:42:02 -0300

    exception livraria

---
+++ 0x03/livraria/exceptions/LivroInvalidoException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class LivroInvalidoException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public LivroInvalidoException(String msg) {
+        super(msg);
+    }
+}
