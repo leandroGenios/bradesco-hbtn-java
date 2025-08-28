cb3f304944c7f0ccd1579430c36f2b42a3182704
Leandro Moreira <leandro.genios@gmail.com>
2025-08-27 21:24:19 -0300

    teste

--- Interfaces/workflow/ProcessadorVideo.java
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -2,24 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProcessadorVideo {
+    private List<CanalNotificacao> canais;
 
-	private List<CanalNotificacao> canais;
-	
-	
-	
-	public ProcessadorVideo() {
-		super();
-		canais = new ArrayList<CanalNotificacao>();
-	}
+    public ProcessadorVideo() {
+        this.canais = new ArrayList<>();
+    }
 
-	public void registrarCanal (CanalNotificacao canal) {
-		canais.add(canal);
-	}
-	
-	public void processar(Video video) {
-		for (CanalNotificacao canalNotificacao : canais) {
-			Mensagem m = new Mensagem(String.format("%s - %s",video.getArquivo(), video.getFormato()), TipoMensagem.LOG);
-			canalNotificacao.notificar(m);
-		}
-	}
+    public void registrarCanal(CanalNotificacao canal) {
+        canais.add(canal);
+    }
+
+    public void processar(Video video) {
+        for (CanalNotificacao canalNotificacao : canais) {
+            Mensagem m = new Mensagem(
+                String.format("%s - %s", video.getArquivo(), video.getFormato()),
+                TipoMensagem.LOG
+            );
+            canalNotificacao.notificar(m);
+        }
+    }
 }
